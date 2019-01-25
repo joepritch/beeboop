6f8e98cff7527bb50b262859b499dbbb89c808fa
Joe Pritchett <joempritchett@gmail.com>
2019-01-25 09:36:26 -0800

    add the ability to return a range of numbers from 0 to user input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
+function numberRange(numberInput) {
+  var range = [];
+  for (var i = numberInput; i >= 0; i--) {
+    range.push(i);
+  }
+  console.log(range)
+  rangeTranslator(range)
+}
+
+function rangeTranslator(range) {
+  var modifierNumbers = [3,2,1]
+  for (var i = 0; i < range.length;) {
+    if (modifierNumbers[0] == (range[i])) {
+      alert("the index of the three is " + [i])
+      i++
+    } else {
+      i++
+    }
+  }
+}
+
+
+
+
+
+
 $(document).ready(function(event) {
   $("form#beepBoopForm").submit(function(event) {
     event.preventDefault();
     var numberInput = parseInt($("#numberInput").val());
-    alert(numberInput);
+    numberRange(numberInput);
+
   });
 });
