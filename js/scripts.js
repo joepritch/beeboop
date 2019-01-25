c833c8962b69b583fd0b8212a009c21854faa565
Joe Pritchett <joempritchett@gmail.com>
2019-01-25 12:38:35 -0800

    add styling to the submit button

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,27 @@ function numberRange(numberInput) {
 }
 
 function rangeTranslator(range) {
-  // var modifierNumbers = ["3"]
+  // var translatedString = rangeString.replace(/3/g, "sorry");
+  // console.log(translatedString)
+  var comma = [","]
+  var modifierNumbers = ["3"]
   var rangeString = range.toString();
-  var translatedString = rangeString.replace(/3/g, "sorry");
-  console.log(translatedString)
-  // for (var i = 0; i < rangeString.length;) {
-  //   if (modifierNumbers.includes(rangeString[i])) {
-  //     alert("the index of the three is " + [i])
-  //     i++
-  //   } else {
-  //     i++
-  //   }
-  // }
+  for (var i = 0; i < rangeString.length;) {
+    if (modifierNumbers.includes(rangeString[i])) {
+      alert([i])
+      if (comma.includes(rangeString[i + 1])) {
+        alert("found comma")
+        if (comma.includes(rangeString[i - 1])) {
+          alert("found second comma")
+          rangeString = rangeString.replace(rangeString[i], "sorry");
+        }
+      }
+      i++
+    } else {
+      i++
+    }
+  }
+  console.log(rangeString);
 }
 
 
